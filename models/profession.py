da87541b532890112dc77e00fe5114e83e530538
Okibe Onmeje <Okibeogomola@gmail.com>
2024-06-06 22:39:35 +0100

    added professional

--- models/profession.py
+++ models/profession.py
@@ -4,9 +4,3 @@ from models.base_model import BaseModel
 
 class Profession(BaseModel):
     '''profession class'''
-    def __init__(self, *args, **kwargs):
-        '''Set instance attributes'''
-        if kwargs:
-            super.__init__(*args, **kwargs)
-        else:
-            print("Please state profession")
