4c9de77c3423453b8341a7b049f9784639785cd9
Okibe Onmeje <Okibeogomola@gmail.com>
2024-09-14 19:43:08 +0100

    fix prevent engine creation without db selectio

--- models/__init__.py
+++ models/__init__.py
@@ -3,14 +3,13 @@ from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DB_storage
 from os import getenv
 
+
 storage_type = getenv("STORAGE_ENGINE")
+
 if storage_type == 'db':
-    storage = DB_storage(
-        #host=getenv("HOST"),
-        #user=getenv("USER"),
-        #password=getenv("PASSWORD"),
-        #db=getenv("DB")
-    )
-else:
+    storage = DB_storage()
+elif storage_type == 'fs':
     storage = FileStorage()
-storage.reload()
+'''if a storage engine  is provided then make tables'''
+if storage_type:
+    storage.reload()
